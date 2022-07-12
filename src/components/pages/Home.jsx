67a6a99dd430c79b650b6bef3d6f0301644bd51a
rajpaul8 <rajpaul83@gmail.com>
2022-07-12 15:05:03 +0530

    Added SearchBar UI in the Home Page

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import UserResults from "../users/UserResults";
+import UserSearch from "../users/UserSearch";
 function Home() {
   return (
     <>
-      {/* Searchbar as a component */}
+      <UserSearch></UserSearch>
       <UserResults></UserResults>
     </>
   );
--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useContext } from "react";
 import Spinner from "../layout/Spinner";
-import UserItem from "../UserItem";
+import UserItem from "./UserItem";
 import GithubContext from "../context/github/GithubContext";
 
 function UserResults() {
